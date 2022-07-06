df3bf8a948457b4af9d7777d27dd26cd89da0643
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-07-06 18:35:18 +0530

    portfolio- portfolio initial commit

--- portfolio website/script/pages.js
+++ portfolio website/script/pages.js
@@ -4,12 +4,34 @@ var home = document.querySelector(".home");
 var aboutBtn = document.querySelector(".about-btn");
 var about = document.querySelector(".about");
 
-aboutBtn.addEventListener("click", () => {
-    home.style.display = "none";
-    about.style.display = "flex";
-});
+var portfolioBtn = document.querySelector(".portfolio-btn");
+var portfolio = document.querySelector (".portfolio")
+
+homeBtn.style.borderBottom = "2px solid var(--main-color)";
 
 homeBtn.addEventListener("click", () => {
     about.style.display = "none";
     home.style.display = "flex";
+    portfolio.style.display = "none";
+    homeBtn.style.borderBottom = "2px solid var(--main-color)";
+    aboutBtn.style.borderBottom = "none";
+    portfolioBtn.style.borderBottom = "none";
+});
+
+aboutBtn.addEventListener("click", () => {
+    home.style.display = "none";
+    about.style.display = "flex";
+    portfolio.style.display = "none";
+    aboutBtn.style.borderBottom = "2px solid var(--main-color)";
+    homeBtn.style.borderBottom = "none";
+    portfolioBtn.style.borderBottom = "none";
+});
+
+portfolioBtn.addEventListener("click", () => {
+    about.style.display = "none";
+    home.style.display = "none";
+    portfolio.style.display = "flex";
+    portfolioBtn.style.borderBottom = "2px solid var(--main-color)";
+    aboutBtn.style.borderBottom = "none";
+    homeBtn.style.borderBottom = "none";
 });
