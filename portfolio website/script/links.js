083e366892d0db2ca9e7d11ffe9530e49b31b3a4
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-07-08 08:50:05 +0530

    portfolio- adding all the links

---
+++ portfolio website/script/links.js
@@ -0,0 +1,4 @@
+var viewCVbtn = document.querySelector(".view-cv-btn");
+viewCVbtn.addEventListener("click", () => {
+    window.open("https://drive.google.com/file/d/1Gs2Byoq-DSccmuyTK5Pb-f6qlCI112hM/view");
+});
